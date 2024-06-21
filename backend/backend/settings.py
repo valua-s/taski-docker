d1e92c533a8479884c6a8fd5b09e981d905c9eb6
valua-s <va1.34@yandex.ru>
2024-06-21 00:53:06 +0000

    verificate me full

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'valua-s.ddns.net', '127.0.0.1', '84.252.139.254' , ]
+ALLOWED_HOSTS = ['localhost', 'valua-s.ddns.net', '127.0.0.1', '84.252.139.254']
 
 
 # Application definition
